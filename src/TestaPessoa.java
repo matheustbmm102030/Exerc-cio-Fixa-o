1b1573dad03b21906cbe5eeed2eddad14591a84f
medei <medei@LAPTOP-H8E7BD2M>
2019-04-23 18:42:38 -0300

    Eunasci

---
+++ src/TestaPessoa.java
@@ -0,0 +1,14 @@
+
+public class TestaPessoa {
+    public static void main(String[] args){
+        
+        Pessoa P1=new Pessoa();
+        P1.nome="Eunasci A.";
+        P1.idade=9999;
+        
+        
+        P1.fazAniversario();
+        System.out.println(P1.nome+" "+P1.idade + "...");
+        
+    }
+}
